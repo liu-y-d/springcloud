e9fe66722e5c6704f7375c1416899e2363d75759
Liuyunda <man021436@163.com>
2020-11-15 02:01:22 +0800

    hystrix 服务监控HystrixDashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/lyd/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.lyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author Liuyunda
+ * @Date 2020/11/15 1:40
+ * @Email man021436@163.com
+ * @Description: DOTO
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
